8bb751229ec87793e0deff0ae739e0fae56f11c5
BingNa Yang <bingnayang@gmail.com>
2021-01-16 15:02:53 -0500

    Initial commit

---
+++ sweet-home-hotel/src/main/java/com/hotel/sweethomehotel/SweetHomeHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.sweethomehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHomeHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetHomeHotelApplication.class, args);
+	}
+
+}
---
+++ sweet-home-hotel/src/test/java/com/hotel/sweethomehotel/SweetHomeHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.sweethomehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetHomeHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
